164733dc4670dcf267b9bb6149d79bb9ee146d5a
AhmetCanArslan <ahmetcanarslan25@gmail.com>
2024-07-16 22:31:56 +0300

    added crawling func to main

--- crawl.js
+++ crawl.js
@@ -1,5 +1,22 @@
-const { link } = require('fs');
 const {JSDOM} = require('jsdom');
+
+async function crawlPage(currentURL){
+    console.log(`actively crawling: ${currentURL}`)
+
+    try{
+        const resp = await fetch(currentURL)
+
+        if(resp.status>399){
+            console.log(`error in fetch with status code: ${resp.status} on page: ${currentURL}`)
+            return
+        }
+        console.log(await resp.text())
+    }catch (err){
+        console.log(`error in fetch: ${err.message}, on page ${currentURL}`)
+    }
+   
+
+}
 
 function getURLsFromHTML(htmlBody, baseURL)
 {
@@ -42,5 +59,6 @@ function normalizeURL(urlString) {
 
 module.exports = { 
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 };
