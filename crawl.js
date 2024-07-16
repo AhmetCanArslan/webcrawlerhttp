0d56345a86dec092bd3362003445892e83d6e782
AhmetCanArslan <ahmetcanarslan25@gmail.com>
2024-07-16 21:12:27 +0300

    get urls from html relative and absolute

--- crawl.js
+++ crawl.js
@@ -1,3 +1,25 @@
+const { link } = require('fs');
+const {JSDOM} = require('jsdom');
+
+function getURLsFromHTML(htmlBody, baseURL)
+{
+    const urls = []
+    const dom = new JSDOM(htmlBody, baseURL)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for (const linkElement of linkElements)
+    {
+        if(linkElement.href.slice(0,1) === '/')
+        {
+            //relative
+            urls.push(`${baseURL}${linkElement.href}`)
+        }else{
+            //absolute
+            urls.push(linkElement.href)        
+        }
+    }
+    return urls
+}
+
 function normalizeURL(urlString) {
     const urlObj = new URL(urlString);
     const hostPath =  `${urlObj.hostname}${urlObj.pathname}`;
@@ -8,5 +30,6 @@ function normalizeURL(urlString) {
 }
 
 module.exports = { 
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 };
